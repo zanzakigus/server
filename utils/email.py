05f6bf9907b6ea4a9d9a175a9d1ca832c90b3c49
Cova-ops <dacovasan00@gmail.com>
2022-02-18 09:09:08 -0600

    Restart password

--- utils/email.py
+++ utils/email.py
@@ -65,3 +65,61 @@ def send_code_password(correo: str, nombre: str, numeros: str):
   
   server.sendmail(EMAIL, correo, bodyEmail.as_string())
   server.quit()
+  
+def send_random_password(correo: str, nombre: str, new_password: str):
+  server = smtplib.SMTP("smtp.gmail.com", 587)
+  server.starttls()
+  server.login(EMAIL, PASSWORD)
+  
+  subject = u"Recuperación de contraseña"
+  
+  bodyEmail = MIMEMultipart("alternative")
+  bodyEmail['From'] = EMAIL
+  bodyEmail['To'] = correo
+  bodyEmail['Subject'] = Header(subject, coding)
+  
+  html = f'''
+    <html>
+      <body>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0" style="background-color: #219653;border-radius:10px">
+          <tr>
+            <td width="15%" align="center" valign="top"></td>
+            <td width="70%" align="center" valign="top">
+              <div style="background-color:#FFFFFF;margin-top:50px;margin-bottom:50px;border-radius:10px;padding:10px">
+                <div style="height:30px;width:10px"></div>
+                <strong style="font-size: 1.6rem">
+                  &iexcl;Hola {nombre}!
+                </strong>
+                <div style="height:1px;width:90%;background-color:#000000;margin-top:3px"></div>
+                <div style="height:20px;width:10px"></div>
+                <p style="font-size: 1.2rem">
+                  Se ha solicitado la restarauci&oacute;n de su contrase&ntilde;a en la aplicaci&oacute;n de Resilience.
+                </p>
+                <p style="font-size: 1.2rem">
+                  Su contrase&ntilde;a ha sido modificada, para ingresar a la aplicación favor de hacerlo con la siguiente contrase&ntilde;a.
+                </p>
+                <strong style="font-size: 1.6rem">
+                  {new_password}
+                </strong>
+                <p style="font-size: 1.2rem">
+                  Si se presenta alg&uacute;n error favor de mandar correo a {EMAIL} para dar soluci&oacute;n.
+                </p>
+                <p style="font-size: 1.2rem">
+                  <em>
+                    El equipo de Resilience le desea un excelente día.
+                  </em>
+                </p>
+              </div>
+            </td>
+            <td width="15%" align="center" valign="top"></td>
+          </tr>
+        </table>
+      </body>
+    </html>
+  '''
+  
+  part_html = MIMEText(html, "html")
+  bodyEmail.attach(part_html)
+  
+  server.sendmail(EMAIL, correo, bodyEmail.as_string())
+  server.quit()
